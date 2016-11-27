1ba2bbc6cbb72678a0c2266ace4c54de0d277402
Kern Ranjitsingh <kernranjitsingh@Kerns-MacBook-Pro.local>
2016-11-26 23:23:08 -0600

    Initial Commit

---
+++ SaveKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveKittens
+//
+//  Created by Kern Ranjitsingh on 2016-11-26.
+//  Copyright © 2016 Kern Ranjitsingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
